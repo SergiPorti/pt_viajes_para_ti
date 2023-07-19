2fb4a2a3eab78c97b95668176e85e8cf7fbfedda
SergiPorti <serporterolo01@gmail.com>
2023-07-19 19:12:11 +0200

    Created Supplier modal

---
+++ src/Entity/Supplier.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SupplierRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SupplierRepository::class)
+ */
+class Supplier
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
