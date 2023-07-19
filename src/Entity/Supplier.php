903bcc7ab0ad9040d4b5f48701d4a7437139d669
SergiPorti <serporterolo01@gmail.com>
2023-07-19 23:35:19 +0200

    Refactor Modal + created Migration + little change in .env

--- src/Entity/Supplier.php
+++ src/Entity/Supplier.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use App\Repository\SupplierRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -10,6 +9,24 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Supplier
 {
+
+    /**
+     * Supplier constructor.
+     * @param $name
+     * @param $email
+     * @param $phone_number
+     * @param $supplier_type
+     * @param $date
+     */
+    public function __construct($name, $email, $phone_number, $supplier_type, $dateTime)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->phone_number = $phone_number;
+        $this->supplier_type = $supplier_type;
+        $this->dateTime= $dateTime;
+    }
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -17,8 +34,110 @@ class Supplier
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $phone_number;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $supplier_type;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_active;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $dateTime;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phone_number;
+    }
+
+    public function setPhoneNumber(string $phone_number): self
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    public function getSupplierType(): ?string
+    {
+        return $this->supplier_type;
+    }
+
+    public function setSupplierType(string $supplier_type): self
+    {
+        $this->supplier_type = $supplier_type;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setIsActive(bool $is_active): self
+    {
+        $this->is_active = $is_active;
+
+        return $this;
+    }
+
+    public function getDateTime(): ?\DateTimeInterface
+    {
+        return $this->dateTime;
+    }
+
+    public function setDateTime(\DateTimeInterface $dateTime): self
+    {
+        $this->dateTime = $dateTime;
+
+        return $this;
     }
 }
