68156f7739728bda452dd95b51d5c1465c692bfb
SergiPorti <serporterolo01@gmail.com>
2023-07-19 19:11:57 +0200

    Created SupplierController

---
+++ src/Controller/SupplierController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SupplierController extends AbstractController
+{
+    /**
+     * @Route("/supplier", name="app_supplier")
+     */
+    public function index(): Response
+    {
+        return $this->render('supplier/index.html.twig', [
+            'controller_name' => 'SupplierController',
+        ]);
+    }
+}
