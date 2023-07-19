a322e96ce3b85283ef3a4d2b0470f624faa5ce90
SergiPorti <serporterolo01@gmail.com>
2023-07-19 23:35:39 +0200

    Created SupplierController

--- src/Controller/SupplierController.php
+++ src/Controller/SupplierController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class SupplierController extends AbstractController
 {
@@ -16,5 +17,20 @@ class SupplierController extends AbstractController
         return $this->render('supplier/index.html.twig', [
             'controller_name' => 'SupplierController',
         ]);
+    }
+
+    public function createSupplier(Request $request): Response
+    {
+        return $this->render(''); //Canviar
+    }
+
+    public function updateSupplier(Request $request): Response
+    {
+        return $this->render('');
+    }
+
+    public function deleteSupplier(Request $request): Response
+    {
+        return $this-> render('');
     }
 }
